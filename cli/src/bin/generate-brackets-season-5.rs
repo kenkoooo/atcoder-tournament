46253ed5c779cfbe6504b99d85f326987a6eb4f0
kenkoooo <kenkou.n@gmail.com>
2021-07-24 22:44:17 +0900

    Add ABC211

--- cli/src/bin/generate-brackets-season-5.rs
+++ cli/src/bin/generate-brackets-season-5.rs
@@ -3,7 +3,21 @@ use cli::bracket_generator::generate_bracket_file;
 use cli::HarmonicMean;
 
 fn main() -> Result<()> {
-    generate_bracket_file::<HarmonicMean>(5, "SSRS", true, &vec![])?;
+    generate_bracket_file::<HarmonicMean>(
+        5,
+        "SSRS",
+        true,
+        &vec![(
+            "./data/season-5/abc211.json",
+            vec![
+                "kyopro_friends",
+                "physics0523",
+                "sugarrr",
+                "tatyam",
+                "blackyuki",
+            ],
+        )],
+    )?;
 
     Ok(())
 }
