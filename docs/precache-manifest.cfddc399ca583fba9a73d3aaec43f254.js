2a74a7e1afe1b00b1169e3b68adf29502518900d
kenkoooo <kenkou.n@gmail.com>
2022-03-26 15:59:47 +0900

    update ratings

---
+++ docs/precache-manifest.cfddc399ca583fba9a73d3aaec43f254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ab4571438103b0cdcc",
+    "url": "/42d5cf6688cbb10d53b3.module.wasm"
+  },
+  {
+    "revision": "53eb6118e076742576c0ee31dc23dbdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "981226090374277a169b",
+    "url": "/static/js/2.63101748.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.63101748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ab4571438103b0cdcc",
+    "url": "/static/js/3.cae17f98.chunk.js"
+  },
+  {
+    "revision": "9428b15b2569ad9b1af6",
+    "url": "/static/js/main.dcac8a04.chunk.js"
+  },
+  {
+    "revision": "dd7c4546977a1e5070eb",
+    "url": "/static/js/runtime-main.f2a7247e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b24fd2311c91c236da7999ff5ab40e6d.js"
+  "/precache-manifest.cfddc399ca583fba9a73d3aaec43f254.js"
 );
 
 self.addEventListener('message', (event) => {
